2bcf83f43fda60fa1d8629f58ad72ec158115c62
Sean Nam <namsangwoo1@gmail.com>
2017-10-07 18:20:18 -0700

    Updated UI

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Submitted by: **Sean Nam**
 
-Time spent: **1.5** hours spent in total
+Time spent: **3** hours spent in total
 
 ## User Stories
 
@@ -17,11 +17,9 @@ The following **optional** features are implemented:
 * [ ] UI animations
 * [ ] Remembering the bill amount across app restarts (if <10mins)
 * [ ] Using locale-specific currency and currency thousands separators.
-* [ ] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
+* [x] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
 
 The following **additional** features are implemented:
-
-- [ ] List anything else that you can get done to improve the app functionality!
 
 ## Video Walkthrough 
 
--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var div2: UILabel!
+    @IBOutlet weak var div3: UILabel!
+    @IBOutlet weak var div4: UILabel!
+    @IBOutlet weak var div5: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +28,11 @@ class ViewController: UIViewController {
         tipControl.selectedSegmentIndex = tipDefaultIndex;
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        // show keyboard on start
+        billField.becomeFirstResponder()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,18 +44,21 @@ class ViewController: UIViewController {
     
     @IBAction func calculateTip(_ sender: AnyObject) {
         let tipPercentages = [0.15, 0.18, 0.20];
-        
-        
-        
+
+        // calculate tip and total based on bill
         let bill = Double(billField.text!) ?? 0;
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex];
         let total = bill + tip;
         
-        
+        // update tip and total labels
         tipLabel.text = String(format: "$%.2f", tip);
         totalLabel.text = String(format: "$%.2f", total);
-
+        
+        // calculate values for sharing
+        div2.text = String(format: "$%.2f", total/2);
+        div3.text = String(format: "$%.2f", total/3);
+        div4.text = String(format: "$%.2f", total/4);
+        div5.text = String(format: "$%.2f", total/5);
     }
 
 }
-
